745fcf5dd4d903f13fc0092b2c9ee2cef6b79607
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-01-04 15:54:24 +0430

    updated and new

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const form=document.getElementById("form");
 const btn=document.getElementById("btn");
 const addBtn=document.getElementById("addBtn");
+const bookList=[];
 addBtn.addEventListener("click",(event)=>{
    event.preventDefault;
 const bookName= document.getElementById("name").value;
 const bookImage= document.getElementById("image").value;
 const bookDescription =document.getElementById("description").value;
+const bookInfo={
+   name: bookName,
+   image: bookImage,
+   desc: bookDescription
+}
+bookList.push(bookInfo)
+  form.classList.remove("flex");
+form.classList.add("hidden"); 
 })
 
 btn.addEventListener("click", ()=>{
